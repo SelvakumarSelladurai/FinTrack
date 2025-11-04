9131eb2ad77652df8e9e3ecd50489b048d04c6c3
SelvakumarBFX <selvakumar@bfxinfotech.com>
2025-11-04 10:36:50 +0530

    feat : Folder structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      <div className="card w-96 bg-base-100 shadow-xl p-6">
+        <h1 className="text-3xl font-bold mb-2 text-center text-primary">
+          Expense Tracker 💰
+        </h1>
+        <p className="text-center opacity-70">
+          TailwindCSS + daisyUI setup successful!
+        </p>
+        <button className="btn btn-primary mt-4 w-full">Get Started</button>
+      </div>
+    </div>
+  );
+}
